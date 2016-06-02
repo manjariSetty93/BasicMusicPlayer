4686c6d1b03495287aaefa83e3c2bdf875cdc6dc
Manjari Setty <setty.manjari@gmail.com>
2016-06-02 14:07:04 -0700

    player with simple notification (no media controller like functions in notification)

---
+++ app/src/main/java/com/example/manjari/musicplayer/MusicController.java
@@ -0,0 +1,18 @@
+package com.example.manjari.musicplayer;
+
+import android.content.Context;
+import android.widget.MediaController;
+
+
+/**
+ * Created by Manjari on 6/1/2016.
+ */
+public class MusicController extends MediaController {
+
+    public MusicController(Context c){
+        super(c);
+    }
+
+    public void hide(){}
+
+}
---
+++ app/src/main/java/com/example/manjari/musicplayer/Song.java
@@ -0,0 +1,27 @@
+package com.example.manjari.musicplayer;
+
+/**
+ * Created by Manjari on 6/1/2016.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){
+        return id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getArtist(){
+        return artist;
+    }
+
+}
